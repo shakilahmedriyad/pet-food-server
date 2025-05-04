75a60fd322bc00db199bf065d0b4730c972f9fe0
shakil1886 <shakil@trynota.ai>
2025-05-05 01:54:56 +0600

    add

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import cors from "cors";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({ allowedHeaders: "*", origin: "*" }));
 app.use(express.json());
 
 app.use("/api", router);
